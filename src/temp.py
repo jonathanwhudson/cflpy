78845debb93bdc233502efff278be177feafdec4
jonathanwhudson <46339754+jonathanwhudson@users.noreply.github.com>
2022-06-29 21:48:11 -0600

    Working to the point of being able to use a model to predict ep/wp

--- src/temp.py
+++ src/temp.py
@@ -8,6 +8,9 @@ from ratelimit import limits, RateLimitException
 from backoff import on_exception
 from urllib.parse import urljoin
 
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
 import json
 import os
 import pathlib
@@ -213,7 +216,7 @@ def load_game_advanced(year: int, game_id: int) -> dict:
 
 
 def setup() -> None:
-    download_games_basic()
+    download_games_basic(reset=False)
     games = load_games_basic(YEAR_START_GAMES, YEAR_END_GAMES)
     games_df = dataframe_games(games)
     download_games_advanced(games_df, YEAR_START_ADV, YEAR_END_ADV, reset=False)
@@ -239,8 +242,11 @@ def games_needing_update():
         f'''SELECT * FROM games WHERE year={YEAR_CURRENT} AND NOT event_status_id=4 AND date_start < CURRENT_DATE''',
         connection)
     sql_query = pd.read_sql_query(
-        f'''SELECT * FROM games WHERE year={YEAR_CURRENT} AND event_status_id=4 AND date_start < CURRENT_DATE AND week=2''',
+        f'''SELECT * FROM games WHERE year={YEAR_CURRENT} AND event_type_id=1 AND week=3''',
         connection)
+    #sql_query = pd.read_sql_query(
+    #    f'''SELECT * FROM games WHERE year={YEAR_CURRENT} AND event_status_id=4 AND date_start < CURRENT_DATE AND event_type_id=1 AND week=3 and game_id=6222''',
+    #    connection)
     connection.close()
     return pd.DataFrame(sql_query)
 
@@ -253,8 +259,9 @@ def remove_rows_game_id(table, game_ids):
 
 
 def main() -> None:
-    # setup()
+    #setup()
     games_update_df = games_needing_update()
+    print(games_update_df['game_id'])
     # download_games_advanced_year(games_update_df, YEAR_CURRENT, reset=True)
     download_games_advanced_year(games_update_df, YEAR_CURRENT, reset=False)
     games_adv = load_games_advanced(games_update_df)
@@ -266,26 +273,37 @@ def main() -> None:
         pbp_df = dataframe_playbyplay(games_adv)
 
         pbp_df['ot'] = pbp_df['quarter'] == 5
-
+        #pbp_df['temp1'] = pbp_df['field_position_start'].str[0:0].isalpha()
         pbp_df['temp'] = pbp_df['field_position_start'].str[1:]
+        print(pbp_df['temp'].isnull().values.any())
         pbp_df['temp'].replace([""], np.nan, inplace=True)
         pbp_df['temp'] = pbp_df['temp'].astype('Int64')
-        pbp_df.loc[pbp_df['field_position_start'].str[0] == pbp_df['team_abbreviation'].str[0], 'distance'] = 110 - pbp_df['temp']
-        pbp_df.loc[pbp_df['field_position_start'].str[0] != pbp_df['team_abbreviation'].str[0], 'distance'] = pbp_df['temp']
+        print(pbp_df['temp'].isnull().values.any())
+        pbp_df.loc[pbp_df['field_position_start'].str[0] == pbp_df['team_abbreviation'].str[0], 'distance'] = 110 - \
+                                                                                                              pbp_df[
+                                                                                                                  'temp']
+        pbp_df.loc[pbp_df['field_position_start'].str[0] != pbp_df['team_abbreviation'].str[0], 'distance'] = pbp_df[
+            'temp']
+        print(pbp_df['distance'].isnull().values.any())
         pbp_df['distance'] = pbp_df['distance'].astype('Int64')
 
-        pbp_df.loc[~pbp_df['ot'], 'time_remaining'] = (4-pbp_df['quarter']) * 900 + pbp_df['play_clock_start_in_secs']
+        pbp_df.loc[~pbp_df['ot'], 'time_remaining'] = (4 - pbp_df['quarter']) * 900 + pbp_df['play_clock_start_in_secs']
         pbp_df.loc[pbp_df['ot'], 'time_remaining'] = 0
         pbp_df['time_remaining'] = pbp_df['time_remaining'].astype('int')
 
-        x = pbp_df.merge(games_df, how='left', left_on='game_id', right_on='game_id')[['game_id', 'play_id', 'team_1_team_id',  'team_1_score','team_1_is_at_home', 'team_1_is_winner', 'team_2_team_id','team_2_abbreviation', 'team_2_score','team_2_is_at_home', 'team_2_is_winner']]
-        pbp_df.loc[x['team_1_team_id']==pbp_df['team_id'], 'won'] = x['team_1_is_winner']
-        pbp_df.loc[x['team_2_team_id']==pbp_df['team_id'], 'won'] = x['team_2_is_winner']
+        x = pbp_df.merge(games_df, how='left', left_on='game_id', right_on='game_id')[
+            ['game_id', 'play_id', 'team_1_team_id', 'team_1_score', 'team_1_is_at_home', 'team_1_is_winner',
+             'team_2_team_id', 'team_2_abbreviation', 'team_2_score', 'team_2_is_at_home', 'team_2_is_winner']]
+        pbp_df.loc[x['team_1_team_id'] == pbp_df['team_id'], 'won'] = x['team_1_is_winner']
+        pbp_df.loc[x['team_2_team_id'] == pbp_df['team_id'], 'won'] = x['team_2_is_winner']
         pbp_df['won'] = pbp_df['won'].astype('bool')
-        pbp_df.loc[x['team_1_team_id']==pbp_df['team_id'], 'home'] = x['team_1_is_at_home']
-        pbp_df.loc[x['team_2_team_id']==pbp_df['team_id'], 'home'] = x['team_2_is_at_home']
-        pbp_df.loc[x['team_1_team_id']==pbp_df['team_id'] & ~x['team_1_is_at_home'] & ~x['team_2_is_at_home'], 'home'] = False
-        pbp_df.loc[x['team_2_team_id']==pbp_df['team_id'] & ~x['team_1_is_at_home'] & ~x['team_2_is_at_home'], 'home'] = True
+        pbp_df.loc[x['team_1_team_id'] == pbp_df['team_id'], 'home'] = x['team_1_is_at_home']
+        pbp_df.loc[x['team_2_team_id'] == pbp_df['team_id'], 'home'] = x['team_2_is_at_home']
+        pbp_df.loc[x['team_1_team_id'] == pbp_df['team_id'] & ~x['team_1_is_at_home'] & ~x[
+            'team_2_is_at_home'], 'home'] = False
+        pbp_df.loc[
+            x['team_2_team_id'] == pbp_df['team_id'] & ~x['team_1_is_at_home'] & ~x['team_2_is_at_home'], 'home'] = True
+        del x
         pbp_df['home'] = pbp_df['home'].astype('bool')
         pbp_df.loc[pbp_df['home'], 'score_diff'] = pbp_df['team_home_score'] - pbp_df['team_visitor_score']
         pbp_df.loc[~pbp_df['home'], 'score_diff'] = pbp_df['team_visitor_score'] - pbp_df['team_home_score']
@@ -298,22 +316,152 @@ def main() -> None:
         pbp_df['score_diff_calc'] = pbp_df['score_diff'] / np.sqrt(pbp_df['time_remaining'] + 1)
         pbp_df['score_diff_calc'] = pbp_df['score_diff_calc'].astype('float')
 
-        pbp_df.loc[pbp_df['play_type_id']==4, 'kickoff'] = True
-        pbp_df.loc[pbp_df['play_type_id']!=4, 'kickoff'] = False
-        pbp_df['kickoff'] = pbp_df['kickoff'].astype('boolean')
+        pbp_df.loc[pbp_df['play_type_id'] == 4, 'kickoff'] = True
+        pbp_df.loc[pbp_df['play_type_id'] != 4, 'kickoff'] = False
+        pbp_df['kickoff'] = pbp_df['kickoff'].astype('bool')
 
-        pbp_df.loc[pbp_df['play_type_id']==3, 'conv1'] = True
-        pbp_df.loc[pbp_df['play_type_id']!=3, 'conv1'] = False
-        pbp_df['conv1'] = pbp_df['conv1'].astype('boolean')
+        pbp_df.loc[pbp_df['play_type_id'] == 3, 'conv1'] = True
+        pbp_df.loc[pbp_df['play_type_id'] != 3, 'conv1'] = False
+        pbp_df['conv1'] = pbp_df['conv1'].astype('bool')
 
-        pbp_df.loc[pbp_df['play_type_id']==8 , 'conv2'] = True
-        pbp_df.loc[pbp_df['play_type_id']==9 , 'conv2'] = True
-        pbp_df.loc[(pbp_df['play_type_id']!=8) & (pbp_df['play_type_id']!=9), 'conv2'] = False
-        pbp_df['conv2'] = pbp_df['conv2'].astype('boolean')
+        pbp_df.loc[pbp_df['play_type_id'] == 8, 'conv2'] = True
+        pbp_df.loc[pbp_df['play_type_id'] == 9, 'conv2'] = True
+        pbp_df.loc[(pbp_df['play_type_id'] != 8) & (pbp_df['play_type_id'] != 9), 'conv2'] = False
+        pbp_df['conv2'] = pbp_df['conv2'].astype('bool')
 
         pbp_df.loc[~pbp_df['kickoff'] & ~pbp_df['conv1'] & ~pbp_df['conv2'], 'regular'] = True
         pbp_df.loc[pbp_df['kickoff'] | pbp_df['conv1'] | pbp_df['conv2'], 'regular'] = False
-        pbp_df['regular'] = pbp_df['regular'].astype('boolean')
+        pbp_df['regular'] = pbp_df['regular'].astype('bool')
+
+        pbp_df['drive_id'] = np.nan
+        pbp_df['drive_sequence'] = np.nan
+        pbp_df['points_scored'] = np.nan
+        pbp_df.sort_values(by=['game_id','play_sequence'],inplace=True)
+        drive_id = 1
+        drive_sequence = 1
+        game_id = None
+        home_score = None
+        visitor_score = None
+        for index, row in pbp_df.iterrows():
+            if game_id is None or row['game_id'] != game_id:
+                drive_sequence = 1
+                game_id = row['game_id']
+                home_score = None
+                visitor_score = None
+            pbp_df.at[index,'drive_id'] = drive_id
+            pbp_df.at[index,'drive_sequence'] = drive_sequence
+            #if row['play_type_id'] == 0 and ("End of Quarter" in row['play_summary'] or "Three Minute" in row['play_summary'] or "Game Delayed" in row['play_summary'] or "Timeout" in row['play_summary'] ):
+                #pbp_df.at[index,'drive_id'] = np.nan
+                #pbp_df.at[index,'drive_sequence'] = np.nan
+            if row['play_type_id'] == 4:
+                drive_id += 1
+                drive_sequence += 1
+                #pbp_df.at[index,'drive_id'] = np.nan
+                #pbp_df.at[index,'drive_sequence'] = np.nan
+            # CFL CONNECT DOESN'T INCLUDE THIS IN DRIVE BUT DOES INCLUDE 2PT attempts? ODD
+            #if row['play_type_id'] == 3:
+            #    pbp_df.at[index,'drive_id'] = np.nan
+            #    pbp_df.at[index,'drive_sequence'] = np.nan
+            # TD
+            elif row['play_result_type_id'] == 1:
+                drive_id += 1
+                drive_sequence += 1
+            #FG
+            elif row['play_result_type_id'] == 2:
+                drive_id += 1
+                drive_sequence += 1
+            #SAFETY
+            elif row['play_result_type_id'] == 10:
+                drive_id += 1
+                drive_sequence += 1
+            # CONVERT ATTEMPT
+            elif row['play_type_id'] in [3,8,9]:
+                drive_id += 1
+                drive_sequence += 1
+            # END HALF
+            elif row['play_result_type_id'] == 8:
+                drive_id += 1
+                drive_sequence += 1
+            # POSSESSION CHANGE (IGNORE CONVERT ATTEMPTS)
+            if row['play_change_of_possession_occurred'] and row['play_type_id'] in [1,2,6,7]:
+                drive_id += 1
+                drive_sequence += 1
+            if home_score is None or visitor_score is None:
+                home_score = row['team_home_score']
+                visitor_score = row['team_visitor_score']
+                if home_score !=0 or visitor_score != 0:
+                    if row['home']:
+                        pbp_df.at[index, 'points_scored'] = home_score - visitor_score
+                    else:
+                        pbp_df.at[index, 'points_scored'] = visitor_score - home_score
+            else:
+                if row['home']:
+                    pbp_df.at[index, 'points_scored'] = (row['team_home_score']-home_score) - (row['team_visitor_score']-visitor_score)
+                else:
+                    pbp_df.at[index, 'points_scored'] = (row['team_visitor_score']-visitor_score) -  (row['team_home_score']-home_score)
+                home_score = row['team_home_score']
+                visitor_score = row['team_visitor_score']
+
+        pbp_df['drive_id'] = pbp_df['drive_id'].astype('Int64')
+        pbp_df['drive_sequence'] = pbp_df['drive_sequence'].astype('Int64')
+        pbp_df['points_scored'] = pbp_df['points_scored'].astype('Int64')
+        #grouped = pbp_df[['drive_sequence','game_id','points_scored']].groupby(by=["game_id",'drive_sequence'], as_index=False).aggregate([np.sum])
+        grouped = pbp_df[['game_id','drive_sequence','points_scored']].groupby(by=['game_id','drive_sequence']).apply(sum)[['points_scored']]
+        grouped = grouped.reset_index()
+
+        pbp_df = pbp_df.merge(grouped, how='left', left_on=['game_id','drive_sequence'], right_on=['game_id','drive_sequence'])
+        pbp_df.rename(columns={"points_scored_x": "points_scored", "points_scored_y": "points_scored_on_drive"}, inplace=True)
+        pbp_df['points_scored_on_drive'] = pbp_df['points_scored_on_drive'].astype('Int64')
+        model1 = pbp_df[["kickoff", "conv1", "conv2", "regular",
+                "distance", "score_diff", "score_diff_calc", "total_score",
+                 "time_remaining", "down", "yards_to_go", "ot", "quarter","points_scored_on_drive"]]
+        model2 = pbp_df[["kickoff", "conv1", "conv2", "regular",
+                "distance", "score_diff", "score_diff_calc", "total_score",
+                 "time_remaining", "down", "yards_to_go", "ot", "quarter","won"]]
+        model1 = model1.dropna()
+        model2 = model2.dropna()
+        model1['distance'] = model1['distance'].astype('int')
+        model2['distance'] = model2['distance'].astype('int')
+        model1['points_scored_on_drive'] = model1['points_scored_on_drive'].astype('int')
+        model1 = pd.concat([model1,pd.get_dummies(model1['down'],prefix="down")],axis=1)
+        model2 = pd.concat([model2,pd.get_dummies(model2['down'],prefix="down")],axis=1)
+        model1 = pd.concat([model1,pd.get_dummies(model1['quarter'],prefix="q")],axis=1)
+        model2 = pd.concat([model2,pd.get_dummies(model2['quarter'],prefix="q")],axis=1)
+        model1.drop("down", axis=1)
+        model1.drop("quarter", axis=1)
+        model2.drop("down", axis=1)
+        model2.drop("quarter", axis=1)
+
+
+
+
+        def split_model_data(x, y: np.array, seed: int):
+            return train_test_split(x, y, stratify=y, test_size=0.3, random_state=seed)
+
+        points_scored_on_drive = np.array(model1.pop('points_scored_on_drive'))
+        train, test, train_labels, test_labels = split_model_data(model1, points_scored_on_drive, 12345)
+        model_ep = RandomForestRegressor(n_estimators=500,
+                                       min_samples_split=2,
+                                       max_leaf_nodes=200,
+                                       random_state=12345,
+                                       max_features='sqrt',
+                                       n_jobs=-1, verbose=0)
+        model_ep.fit(train, train_labels)
+
+        won = np.array(model2.pop('won'))
+        train, test, train_labels, test_labels = split_model_data(model2, won, 12345)
+        model_wp = RandomForestClassifier(n_estimators=500,
+                                       min_samples_split=2,
+                                       max_leaf_nodes=200,
+                                       random_state=12345,
+                                       max_features='sqrt',
+                                       n_jobs=-1, verbose=0)
+        model_wp.fit(train, train_labels)
+
+        model_ep.predict(model1)
+        model_wp.predict(model2)
+
+
 
         exit()
 
@@ -331,21 +479,23 @@ def main() -> None:
             for name, df in value.items():
                 remove_rows_game_id(f"{type}_{name}", game_ids)
 
-        store_dataframe(games_df, "games", 'append')
-        store_dataframe(pbp_df, "play_by_play", 'append')
-        store_dataframe(pen_df, "penalties", 'append')
-        store_dataframe(rev_df, "play_reviews", 'append')
-        store_dataframe(ros_df, "roster", 'append')
-        for type, value in result_dict_dfs.items():
-            for name, df in value.items():
-                store_dataframe(df, f"{type}_{name}", 'append')
+
+
+        #store_dataframe(games_df, "games", 'append')
+        #store_dataframe(pbp_df, "play_by_play", 'append')
+        #store_dataframe(pen_df, "penalties", 'append')
+        #store_dataframe(rev_df, "play_reviews", 'append')
+        #store_dataframe(ros_df, "roster", 'append')
+        #for type, value in result_dict_dfs.items():
+        #    for name, df in value.items():
+        #        store_dataframe(df, f"{type}_{name}", 'append')
 
     else:
         print("No games to update!")
 
+
 # entry
 # drive_sequence play
-# regular play
 # points_on_drive play
 # wp play
 # ep play
